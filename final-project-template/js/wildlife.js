6ec051c59356b1a7edf82a9889e13d0ffebcf3d0
archie49 <S02165572@FRCCinst.ccofc.edu>
2017-04-25 20:56:37 -0600

    Data add
    Added base map and basic data to get started.

--- final-project-template/js/wildlife.js
+++ final-project-template/js/wildlife.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    /*
-    your code will go here
-    */
-    
+ var map = L.map('map').setView([40.38, -105.5], 4);   
+L.tileLayer('https://api.mapbox.com/styles/v1/archie21/cj1gmpwzo000w2rnwph8quk42/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYXJjaGllMjEiLCJhIjoiY2l5MmNpeXRjMDAxbTJxcWphOWc0cjNjYSJ9.J3pcJEVhPcdyS1-Apr2kgg',
+{
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
+}).addTo(map);
+L.esri.featureLayer({
+    url: 'https://services.nationalmap.gov/arcgis/rest/services/transportation/MapServer/41'
+  }).addTo(map);
 });
